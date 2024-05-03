a329d65095a23ce376a664f39d7418a2d874eb12
ishsarin <ishsarin2000@gmail.com>
2024-05-03 18:39:55 +0530

    completed project

--- vite-project/src/context/LangContextProvider.jsx
+++ vite-project/src/context/LangContextProvider.jsx
@@ -1,8 +1,15 @@
 import React, { createContext, useContext, useState } from "react";
-
+import { useEffect } from "react";
 const LangContext = createContext();
 const LangContextProvider = ({ children }) => {
-  const [lang, setLang] = useState("");
+  // const [lang, setLang] = useState("");
+  const [lang, setLang] = useState(() => {
+    return JSON.parse(localStorage.getItem("lang")) || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("lang", JSON.stringify(lang));
+  }, [lang]);
 
   return (
     <LangContext.Provider value={{ lang, setLang }}>
